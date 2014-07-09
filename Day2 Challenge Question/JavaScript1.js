7d8fa1e5e4f04f0d30625a4b49e94e7058a26743
TheSingingCoder <random12@gmail.com>
2014-07-09 09:05:47 -0500

    Day2 Challenge Question

---
+++ Day2 Challenge Question/JavaScript1.js
@@ -0,0 +1,16 @@
+﻿//Challenge Question
+var petArr = [];
+var petPush = function () {
+    petArr.push(document.getElementById("pet-name").value);
+    document.getElementById("pet-name").value = "";
+    console.log(petArr);
+};
+
+var petListFunc = function () {
+    var petString = "";
+    for (var i = 0; i < petArr.length; i++) {
+        petString += petArr[i] + " <br/>";
+    }
+    document.getElementById("pet-list").innerHTML = petString;
+    console.log(petString);
+};
